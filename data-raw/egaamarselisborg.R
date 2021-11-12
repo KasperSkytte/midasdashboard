dd34c2258ab15ca1d5da9fa19467bb83da0dad7d
Kasper Skytte Andersen <knaldhat@gmail.com>
2021-11-12 16:31:26 +0100

    dont save as rda manually

--- data-raw/egaamarselisborg.R
+++ data-raw/egaamarselisborg.R
@@ -2,9 +2,14 @@ source("data-raw/helpers.R")
 
 #egaamarselisborg CP332 hasteprøver
 #data copied from CP-Ongoing 18 may 2018
-egaamarselisborg <- amp_subset_samples(amp_load(otutable = read.delim("data-raw/amplicon_data/aarhusvand CP332/otutable.txt", check.names = FALSE), metadata = openxlsx::read.xlsx("data-raw/amplicon_data/aarhusvand CP332/CP332_metadata.xlsx", rows = 1:21, cols = c(1,9,11))), normalise = TRUE)
+egaamarselisborg <- amp_subset_samples(
+  amp_load(
+    otutable = read.delim("data-raw/amplicon_data/aarhusvand CP332/otutable.txt", check.names = FALSE),
+    metadata = openxlsx::read.xlsx("data-raw/amplicon_data/aarhusvand CP332/CP332_metadata.xlsx", rows = 1:21, cols = c(1,9,11))),
+  normalise = TRUE
+)
 colnames(egaamarselisborg$metadata) <- c("SampleID", "Plant", "Date")
 egaamarselisborg$metadata$Date <- as.Date(egaamarselisborg$metadata$Date, origin = "1899-12-30", format = "%Y-%m-%d")
 egaamarselisborg$metadata <- genusfunctions(egaamarselisborg)
-save(egaamarselisborg, file = "appdata/egaamarselisborg(CP332).rda")
-usethis::use_data(cp332, overwrite = TRUE)
+
+usethis::use_data(egaamarselisborg, overwrite = TRUE)
